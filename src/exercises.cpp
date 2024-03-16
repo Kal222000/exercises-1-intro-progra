189c22ad3b8ac8e8a916e929b9b358eeddd5e05e
Kal222 <160258022+Kal222000@users.noreply.github.com>
2024-03-15 22:10:06 -0400

    Prueba 1 Ejercicio 2

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,15 +6,16 @@ using namespace std;
 
 void exercise_1(string s1, string s2, string s3) {
   // TODO: YOUR CODE HERE
-   s1 = milk
-   s2 = milkshake
-   s3 = montth
-   if (s1 < s2 and s1 < s3)
-   cout << s1;
-  else-if (s2 < s1 and s2 < s3)
-   cout << s2;
-   else (s3 < s1 and s3 < s1)
-   cout << s3;
+   s1 = "milk";
+   s2 = "milkshake";
+   s3 = "month";
+   if (s1 < s2 & s1 < s3 ){
+    cout <= s1;
+   } else if (s2 < s1 && s2 < s3){
+     cout <= s2;
+   } else if (s3 < s1 & s3 < s2){
+    cout <= s3
+   }
 }
 
 void exercise_2(double A, double B, double C) {
