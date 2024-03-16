c85d0acc0d36899cf3bd725488be7224da6c344a
Kal222 <160258022+Kal222000@users.noreply.github.com>
2024-03-16 15:02:11 -0400

    Prueba 6 Ejercicio 1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 void exercise_1(string s1, string s2, string s3) {
    if (s1 < s2 && s1 < s3){
-      cout << s1;
+    cout << s1;
     } 
     else if (s2 < s1 && s2 < s3){
-      cout << s2;
+    cout << s2;
     } 
     else if (s3 < s1 && s3 < s2){
-      cout << s3;
+    cout << s3;
     }
 }
 
