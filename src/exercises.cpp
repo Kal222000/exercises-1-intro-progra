a24a6559d9c2d12cd3c44aec74dc505caad494cb
Kal222 <160258022+Kal222000@users.noreply.github.com>
2024-04-30 11:57:01 -0400

    Completando 5

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -59,21 +59,21 @@ void exercise_5(char character) {
 
 void exercise_6(int number) {
   switch(number){
-      case 1: cout<<"Lunes"<<endl;
+      case 1: cout<<"Monday"<<endl;
       break;
-      case 2: cout<<"Martes"<<endl;
+      case 2: cout<<"Tuesday"<<endl;
       break;
-      case 3: cout<<"Miercoles"<<endl;
+      case 3: cout<<"Wednesday"<<endl;
       break;
-      case 4: cout<<"Jueves"<<endl;
+      case 4: cout<<"Thursday"<<endl;
       break;
-      case 5: cout<<"Viernes"<<endl;
+      case 5: cout<<"Friday"<<endl;
       break;
-      case 6: cout<<"Sabado"<<endl;
+      case 6: cout<<"Saturday"<<endl;
       break;
-      case 7: cout<<"Domingo"<<endl;
+      case 7: cout<<"Sunday"<<endl;
       break;
-      default: cout<<"Invalid input";
+      default: cout<<"Invalid input"<<endl;
       break;
   }
 }
